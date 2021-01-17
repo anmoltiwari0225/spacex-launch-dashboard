65fa9b8a3e0602593b92e161bd1240c30df4b192
Anmol Tiwari <anmoltiwari0225@gmail.com>
2021-01-18 00:30:53 +0530

    More changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,8 +8,8 @@ function Card(props) {
                 <h5><b>Mission Name:</b><span className="mission-details">{props.launch.mission_name !== ''?props.launch.mission_name: "Data not available"}</span></h5>
                 <h5><b>Mission ID:</b> <span className="mission-details">{props.launch.mission_id !== []? props.launch.mission_id: "Data not available"}</span></h5>
                 <h5><b>Launch Year:</b> <span className="mission-details">{props.launch.launch_year !== ''? props.launch.launch_year: "Data not available"}</span></h5>
-                <h5><b>Successful Launch:</b> <span className="mission-details">{props.launch.launch_success !== ''? props.launch.mission_id: "Data not available"}</span></h5>
-                <h5><b>Successful Landing:</b> <span className="mission-details">{props.launch.launch_landing !== ''? props.launch.mission_id: "Data not available"}</span></h5>
+                <h5><b>Successful Launch:</b> <span className="mission-details">{props.launch.launch_success !== ''? props.launch.launch_success: "Data not available"}</span></h5>
+                <h5><b>Successful Landing:</b> <span className="mission-details">{props.launch.launch_landing !== ''? props.launch.launch_landing: "Data not available"}</span></h5>
         </div>
     )
 }
