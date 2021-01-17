71f10715d1156c879a6c5088ab28150c643bb59c
Anmol Tiwari <anmoltiwari0225@gmail.com>
2021-01-18 00:27:05 +0530

    More changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,10 +6,10 @@ function Card(props) {
         <div className="card">
             <img src={props.launch.links.mission_patch} alt={`Mission Patch`} style={{"width":"50%"}} />
                 <h5><b>Mission Name:</b><span className="mission-details">{props.launch.mission_name !== ''?props.launch.mission_name: "Data not available"}</span></h5>
-                <h5><b>Mission ID:</b> <span className="mission-details">{props.launch.mission_id !== ''? props.launch.mission_id: "Data not available"}</span></h5>
+                <h5><b>Mission ID:</b> <span className="mission-details">{props.launch.mission_id !== []? props.launch.mission_id: "Data not available"}</span></h5>
                 <h5><b>Launch Year:</b> <span className="mission-details">{props.launch.launch_year !== ''? props.launch.launch_year: "Data not available"}</span></h5>
-                <h5><b>Successful Launch:</b> <span className="mission-details">{props.launch.mission_id !== ''? props.launch.mission_id: "Data not available"}</span></h5>
-                <h5><b>Successful Landing:</b> <span className="mission-details">{props.launch.mission_id !== ''? props.launch.mission_id: "Data not available"}</span></h5>
+                <h5><b>Successful Launch:</b> <span className="mission-details">{props.launch.launch_success !== ''? props.launch.mission_id: "Data not available"}</span></h5>
+                <h5><b>Successful Landing:</b> <span className="mission-details">{props.launch.launch_landing !== ''? props.launch.mission_id: "Data not available"}</span></h5>
         </div>
     )
 }
