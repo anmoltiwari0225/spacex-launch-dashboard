f054627da8b76a0fcf43ed9d0c0299613c1bfcc4
Anmol Tiwari <anmoltiwari0225@gmail.com>
2021-01-17 21:33:08 +0530

    Server side stuff

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { connect } from 'react-redux';
 
 import Card from './Card';
 import './Dashboard.css';
+import {fetchLaunch} from '../actions/actions';
 
 function Dashboard(props) {
-    
+    useEffect(() => {
+        props.fetchLaunch()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
     return (
         <div className="main">
             {props.launches.map((launch, key) => {
                 return (
-                        <Card key={key} launch={launch} />
+                    <Card key={key} launch={launch} />
                 );
             })}
         </div>
@@ -26,4 +30,10 @@ const mapStateToProps = state => {
     }
 };
 
-export default connect(mapStateToProps, null)(Dashboard)
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchLaunch: () => {dispatch(fetchLaunch())},
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
