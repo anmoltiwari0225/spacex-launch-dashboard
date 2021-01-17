99df53ce59caceea517949ea726027022a11f93d
Anmol Tiwari <anmoltiwari0225@gmail.com>
2021-01-17 12:45:40 +0530

    Added PWA and SSR functionalities

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import {logger} from 'redux-logger';
 
-import mainReducer from './reducers';
+import mainReducer from './reducers/reducers';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import FilterBox from './components/FilterBox';
 
-const store = createStore(mainReducer)
+const store = createStore(mainReducer, applyMiddleware(thunk, logger));
 
 function App() {
   return (
     <Provider store={store}>
       <div>
+        <h2>SpaceX Launch Programs</h2>
         <FilterBox />
         <Dashboard />
       </div>
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
 
-function Dashboard() {
+import Card from './Card';
+
+function Dashboard(props) {
     return (
         <div>
-           Dashboard 
+            <h1>
+                Launch Dashboard   
+            </h1>
+            <div className="row">
+                    {props.launches.map((launch, key) => {
+                        return (
+                            <div className="column">
+                                <Card key={key} launch={launch} />
+                            </div>
+                        );
+                    })}
+            </div>
         </div>
     )
 }
 
-export default Dashboard
+const mapStateToProps = state => {
+    return {
+        launches: state.launches,
+        launchYear: state.launchYear,
+        isLaunchSuccessful: state.isLandingSuccessful,
+        isLandingSuccessful: state.isLandingSuccessful,
+    }
+};
+
+export default connect(mapStateToProps, null)(Dashboard)
